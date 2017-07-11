6841780464c996ee8d2431c225ca2e7247cf3005
analyn <analyncajocson@ymail.com>
2017-07-11 16:36:27 +0800

    improve codes

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var lightButton: UIButton!
     var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +24,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
+        view.backgroundColor = lightOn ? .white : .black
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
